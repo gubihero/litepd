0b4fbe2f064fb0b0e440226049a7200cfc95ed35
James Gubler <gubimail@gmail.com>
2015-03-11 19:42:08 -0600

    more https changes

--- app.js
+++ app.js
@@ -15,23 +15,23 @@
       })
   });
 
-  
+
 
   app.controller('pokemonListController', function($scope, $http){
-    $http.get('http://pokeapi.co/api/v1/pokedex/1/').success(function(data) {
+    $http.get('https://pokeapi.co/api/v1/pokedex/1/').success(function(data) {
       $scope.pokemonlist = data.pokemon;
     });
   });
 
   app.controller('pokemonController', function($scope, $http, $routeParams){
     var pokemonName = $routeParams.pokemonName;
-    $http.get('http://pokeapi.co/api/v1/pokemon/'+pokemonName).success(function(data) {
+    $http.get('https://pokeapi.co/api/v1/pokemon/'+pokemonName).success(function(data) {
       $scope.pokemon = data;
       console.log($scope.pokemon);
-      $http.get('http://pokeapi.co'+$scope.pokemon['sprites'][0]['resource_uri']).success(function(data) {
+      $http.get('https://pokeapi.co'+$scope.pokemon['sprites'][0]['resource_uri']).success(function(data) {
         $scope.image = 'http://pokeapi.co'+data.image;
       });
-      $http.get('http://pokeapi.co'+$scope.pokemon['descriptions'][0]['resource_uri']).success(function(data) {
+      $http.get('https://pokeapi.co'+$scope.pokemon['descriptions'][0]['resource_uri']).success(function(data) {
         $scope.bio = data;
       });
     });
