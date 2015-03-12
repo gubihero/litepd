538d17782cb099884d04545430888e4a73d20853
James Gubler <gubimail@gmail.com>
2015-03-11 18:39:37 -0600

    added angular list and detail views, plus styling

---
+++ app.js
@@ -0,0 +1,40 @@
+(function(){
+  var app = angular.module('pokedexlite', ['ngRoute']);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+        templateUrl : 'templates/pokemonList.html',
+        controller  : 'pokemonListController'
+      })
+
+      .when('/pokemon/:pokemonName', {
+        templateUrl : 'templates/pokemon.html',
+        controller  : 'pokemonController'
+      })
+  });
+
+  
+
+  app.controller('pokemonListController', function($scope, $http){
+    $http.get('http://pokeapi.co/api/v1/pokedex/1/').success(function(data) {
+      $scope.pokemonlist = data.pokemon;
+    });
+  });
+
+  app.controller('pokemonController', function($scope, $http, $routeParams){
+    var pokemonName = $routeParams.pokemonName;
+    $http.get('http://pokeapi.co/api/v1/pokemon/'+pokemonName).success(function(data) {
+      $scope.pokemon = data;
+      console.log($scope.pokemon);
+      $http.get('http://pokeapi.co'+$scope.pokemon['sprites'][0]['resource_uri']).success(function(data) {
+        $scope.image = 'http://pokeapi.co'+data.image;
+      });
+      $http.get('http://pokeapi.co'+$scope.pokemon['descriptions'][0]['resource_uri']).success(function(data) {
+        $scope.bio = data;
+      });
+    });
+  });
+})();
+
